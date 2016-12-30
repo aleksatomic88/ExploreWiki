255b2e796a5463c60a84ae62b26c07341cc0ce55
bojan <kukricar.bojan@gmail.com>
2016-12-30 19:05:33 +0100

    Work in progress - part 1: - Rafactoring code for MVC standar: View, ViewModel, Extensions, Scripts and Styles separated; - Node/edges object can/should be deserilized on client side; - Autocomplete is temporary broken...

--- ExploreWiki/Controllers/HomeController.cs
+++ ExploreWiki/Controllers/HomeController.cs
@@ -1,117 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Configuration;
+using System.Diagnostics;
 using System.Web.Mvc;
+using ExploreWiki.Helpers;
+using ExploreWiki.Models;
+using ExploreWiki.ViewModel;
+using Newtonsoft.Json;
 using QC = System.Data.SqlClient;  // System.Data.dll  
 using DT = System.Data;
-using System.Text;
-using System.Configuration;
 
 namespace ExploreWiki.Controllers
 {
     public class HomeController : Controller
     {
-        public class InputPersonName
-        {
-            public string InputName { get; set; }
-        }
-
-        /// <summary>
-        /// Class representing a node in graph.
-        /// </summary>
-        public class Person
-        {
-            /// <summary>
-            /// Name of the node.
-            /// </summary>
-            public string Name { get; set; }
-
-            /// <summary>
-            /// Unique id of the node.
-            /// </summary>
-            public int Id { get; set; }
-
-            /// <summary>
-            /// Distance from the starting point.
-            /// </summary>
-            public int DistanceFromCenter { get; set; }
-
-            /// <summary>
-            /// Normalized name string.
-            /// </summary>
-            public string NormalizedName { get { return NormalizeString(Name); } }
-
-            /// <summary>
-            /// Color of the node.
-            /// As we go away from the center color changes.
-            /// </summary>
-            public string GetRGBColor
-            {
-                get
-                {
-                    if (DistanceFromCenter == 0)
-                    {
-                        return "rgb(255, 0, 0)";
-                    }
-                    else
-                    {
-                        return string.Format("rgb(155, 155, {0}", Math.Max(200 - 10 * DistanceFromCenter, 70));
-                    }
-                }
-            }
-
-            /// <summary>
-            /// Unique id generation.
-            /// </summary>
-            private static int lastUsedId = 0;
-
-            /// <summary>
-            /// Constructor.
-            /// </summary>
-            /// <param name="name"></param>
-            /// <param name="distanceFromCeneter"></param>
-            public Person(string name, int distanceFromCeneter)
-            {
-                Name = name;
-                Id = lastUsedId++;
-                DistanceFromCenter = distanceFromCeneter;
-            }
-        }
-
         /// <summary>
         /// Max number of nodes in graph.
         /// </summary>
         private const int MaxNumberOfPersonsPerGraph = 100;
 
         private const string DefaultProviderName = "System.Data.ProdDb";
-
-        /// <summary>
-        /// Class representing branch in the graph.
-        /// </summary>
-        public class Relation
-        {
-            public Person PersonFrom { get; set; }
-
-            public Person PersonTo { get; set; }
-
-            public string RelationName { get; set; }
-
-
-            /// <summary>
-            /// Constructor.
-            /// </summary>
-            /// <param name="personFrom"></param>
-            /// <param name="personTo"></param>
-            /// <param name="relationName"></param>
-            public Relation(Person personFrom, Person personTo, string relationName)
-            {
-                PersonFrom = personFrom;
-                PersonTo = personTo;
-                RelationName = relationName;
-            }
-        }
-
+        
         /// <summary>
         /// List of all connections in graph.
         /// </summary>
@@ -128,130 +37,14 @@ namespace ExploreWiki.Controllers
         /// Generating helper message from the time needed to generate the graph.
         /// </summary>
         TimeSpan GenerationTook { get; set; }
-
-        /// <summary>
-        /// String normalization state machine.
-        /// </summary>
-        enum StringNormalizerState
-        {
-            NormalChar,
-            FirstPercentage,
-            FirstSpecialChar,
-            SecondPercentage,
-            SecondSpecialChar,
-        }
-
-        /// <summary>
-        /// Normalize the input string.
-        /// In wiki dump we store info in "url" like format with %20 as space, '%' escaping for unicode etc.
-        /// For now normalization is done here.
-        /// TODO: This should be really normalized in database itself.
-        /// </summary>
-        /// <param name="originalString">String to normalize.</param>
-        /// <returns>Normalized string.</returns>
-        private static string NormalizeString(string originalString)
-        {
-            List<char> normalizedChars = new List<char>();
-            StringNormalizerState state = StringNormalizerState.NormalChar;
-
-            List<char> specialBytes = new List<char>();
-            int specialCharsCounter = 0;
-
-            foreach (char currentChar in originalString)
-            {
-                switch(state)
-                {
-                    case StringNormalizerState.NormalChar:
-                        if (currentChar == '%')
-                        {
-                            state = StringNormalizerState.FirstPercentage;
-                        }
-                        else
-                        {
-                            normalizedChars.Add(currentChar);
-                        }
-                        break;
-                    case StringNormalizerState.FirstPercentage:
-                        specialBytes.Add(currentChar);
-                        specialCharsCounter++;
-
-                        if (specialCharsCounter == 2)
-                        {
-                            state = StringNormalizerState.SecondPercentage;
-                            specialCharsCounter = 0;
-                        }
-
-                        break;
-                    case StringNormalizerState.SecondPercentage:
-                        if (currentChar == '%')
-                        {
-                            state = StringNormalizerState.SecondSpecialChar;
-                        }
-                        else
-                        {
-                            // Something is strange, just return the original.
-                            return originalString;
-                        }
-                        break;
-                    case StringNormalizerState.SecondSpecialChar:
-                        specialBytes.Add(currentChar);
-                        specialCharsCounter++;
-
-                        if (specialCharsCounter == 2)
-                        {
-                            state = StringNormalizerState.NormalChar;
-
-                            // TODO: This is really not performant but will do for now.
-                            byte byte1 = Convert.ToByte(new string(specialBytes.Take(2).ToArray()), 16);
-                            byte byte2 = Convert.ToByte(new string(specialBytes.Skip(2).ToArray()), 16);
-
-                            normalizedChars.AddRange(Encoding.UTF8.GetChars(new byte[] { byte1, byte2 }));
-
-                            specialBytes.Clear();
-                            specialCharsCounter = 0;
-                        }
-
-                        break;
-                }
-            }
-
-            return (new string(normalizedChars.ToArray())).Replace("_", " ");
-        }
-
-        /// <summary>
-        /// Put string into 'database' format.
-        /// </summary>
-        /// <param name="originalString"></param>
-        /// <returns>Denormalized version of input string.</returns>
-        private static string Denormalize(string originalString)
-        {
-            // TODO: when we do normalization in db this shouldn't be here.
-            List<char> denormalizedChars = new List<char>();
-            foreach (char currentChar in originalString)
-            {
-                if ((int)currentChar < 256)
-                {
-                    denormalizedChars.Add(currentChar);
-                }
-                else
-                {
-                    // break it.
-                    denormalizedChars.Add('%');
-                    denormalizedChars.AddRange(
-                        BitConverter.ToString(Encoding.UTF8.GetBytes(new char[] { currentChar })).Replace('-', '%').ToArray()
-                    );
-                }
-            }
-
-            return (new string(denormalizedChars.ToArray())).Replace(" ", "_");
-        }
+       
 
         /// <summary>
         /// Action which builds the home page.
         /// </summary>
-        /// <param name="inputNameModel">Input name from the main form.</param>
+        /// <param name="personViewModel">Input name from the main form.</param>
         /// <returns></returns>
-        public ActionResult Index(InputPersonName inputNameModel)
+        public ActionResult Index(PersonViewModel personViewModel)
         {
             Persons = new Dictionary<string, Person>();
             PersonConnections = new List<Relation>();
@@ -260,18 +53,18 @@ namespace ExploreWiki.Controllers
             ViewBag.PersonConnections = PersonConnections;
             ViewBag.Message = "Enter person name to start graph traversal.";
 
-            if (String.IsNullOrEmpty(inputNameModel.InputName))
+            if (String.IsNullOrEmpty(personViewModel.InputName))
             {
                 // Nothing to do here. Waiting for user input.
                 return View();
             }
 
-            ViewBag.InputName = inputNameModel.InputName;
+            ViewBag.InputName = personViewModel.InputName;
 
-            Person startingPerson = new Person(Denormalize(inputNameModel.InputName), 0);
+            Person startingPerson = new Person(personViewModel.InputName.Denormalize(), 0);
             Persons.Add(startingPerson.Name, startingPerson);
 
-            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            Stopwatch sw = new Stopwatch();
             sw.Start();
             BuildGraph(startingPerson, 1, true, false);
 
@@ -285,12 +78,75 @@ namespace ExploreWiki.Controllers
 
             GenerationTook = sw.Elapsed;
 
+            ViewBag.PersonsJson = JsonConvert.SerializeObject(Persons, Formatting.Indented);
+            ViewBag.PersonsConnectionsJson = JsonConvert.SerializeObject(PersonConnections, Formatting.Indented);
+            
             ViewBag.PersonNames = Persons;
             ViewBag.PersonConnections = PersonConnections;
             ViewBag.GenerationTook = string.Format("{0}s for generating graph of {1} nodes.", GenerationTook.TotalSeconds, Persons.Count);
+            return View(personViewModel);
+        }
+
+        
+        /// <summary>
+        /// Empty for now.
+        /// </summary>
+        /// <param name="personViewModel"></param>
+        /// <returns></returns>
+        public ActionResult About(PersonViewModel personViewModel)
+        {
             return View();
         }
 
+
+        /// <summary>
+        /// Empty for now.
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Contact()
+        {
+            ViewBag.Title = "I should write something here but I am too lazy.";
+
+            return View();
+        }
+
+        /// <summary>
+        /// Auto complete for user inputs.
+        /// </summary>
+        /// <param name="term">Search pattern we are using for.</param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Autocomplete(string term)
+        {
+            // Pulling this from database is a bit too slow.
+            List<string> autocompleteItems = new List<string>();
+            using (var connection = new QC.SqlConnection(GetConnectionString(DefaultProviderName)))
+            {
+                connection.Open();
+                using (var command = new QC.SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandType = DT.CommandType.Text;
+                    command.CommandText = @"  
+                        select top 10 name from persons1 
+                        where name like @name
+                    ";
+
+                    command.Parameters.AddWithValue("@name", term.Denormalize() + "%");
+
+                    QC.SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        autocompleteItems.Add(reader.GetString(0).Normalize());
+                    }
+                }
+            }
+
+            return Json(autocompleteItems.ToArray(), JsonRequestBehavior.AllowGet);
+        }
+
+        #region private_helper_methods
         private string GetConnectionString(string providerName)
         {
             ConnectionStringSettingsCollection settings = ConfigurationManager.ConnectionStrings;
@@ -322,7 +178,7 @@ namespace ExploreWiki.Controllers
         {
             // TODO: Don't open new connection every time.
             // Use connection pool.
-            using (var connection = new QC.SqlConnection( GetConnectionString(HomeController.DefaultProviderName)))
+            using (var connection = new QC.SqlConnection(GetConnectionString(DefaultProviderName)))
             {
                 connection.Open();
                 using (var command = new QC.SqlCommand())
@@ -334,34 +190,34 @@ namespace ExploreWiki.Controllers
                     if (includePointingTo && !includeNonPersons)
                     {
                         command.CommandText = @"  
-select entity_name as node_from, property_name as branch_name, link2 as node_to from properties1 
-where link2 = @name and  exists (select name from persons where name = entity_name)
-select entity_name as node_from, property_name as branch_name, link2 as node_to from properties1 
-where entity_name = @name and link2 is not null and isPerson = 1
-";
+                            select entity_name as node_from, property_name as branch_name, link2 as node_to from properties1 
+                            where link2 = @name and  exists (select name from persons where name = entity_name)
+                            select entity_name as node_from, property_name as branch_name, link2 as node_to from properties1 
+                            where entity_name = @name and link2 is not null and isPerson = 1
+                        ";
                     }
                     else if (!includePointingTo && includeNonPersons)
                     {
 
                         command.CommandText = @"  
-select entity_name as node_from, property_name as branch_name, link2 as node_to from properties1 
-where entity_name = @name and link2 is not null
-";
+                            select entity_name as node_from, property_name as branch_name, link2 as node_to from properties1 
+                            where entity_name = @name and link2 is not null
+                         ";
                     }
                     else if (includePointingTo && includeNonPersons)
                     {
                         command.CommandText = @"
-select entity_name as node_from, property_name as branch_name, link2 as node_to from properties1 
-where (link2 = @name) or (entity_name = @name and link2 is not null)
-";
+                            select entity_name as node_from, property_name as branch_name, link2 as node_to from properties1 
+                            where (link2 = @name) or (entity_name = @name and link2 is not null)
+                        ";
                     }
                     else
                     {
 
                         command.CommandText = @"  
-select entity_name as node_from, property_name as branch_name, link2 as node_to from properties1 
-where entity_name = @name and link2 is not null and isPerson = 1
-";
+                            select entity_name as node_from, property_name as branch_name, link2 as node_to from properties1 
+                            where entity_name = @name and link2 is not null and isPerson = 1
+                        ";
                     }
 
                     command.Parameters.AddWithValue("@name", startingPerson.Name);
@@ -446,67 +302,6 @@ where entity_name = @name and link2 is not null and isPerson = 1
                 }
             }
         }
-
-        /// <summary>
-        /// Empty for now.
-        /// </summary>
-        /// <param name="inputNameModel"></param>
-        /// <returns></returns>
-        public ActionResult About(InputPersonName inputNameModel)
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult UserInput(InputPersonName inputNameModel)
-        {
-            return View();
-        }
-
-        /// <summary>
-        /// Empty for now.
-        /// </summary>
-        /// <returns></returns>
-        public ActionResult Contact()
-        {
-            ViewBag.Title = "I should write something here but I am too lazy.";
-
-            return View();
-        }
-
-        /// <summary>
-        /// Auto complete for user inputs.
-        /// </summary>
-        /// <param name="term">Search pattern we are using for.</param>
-        /// <returns></returns>
-        public ActionResult Autocomplete(string term)
-        {
-            // Pulling this from database is a bit too slow.
-            List<string> autocompleteItems = new List<string>();
-            using (var connection = new QC.SqlConnection(GetConnectionString(HomeController.DefaultProviderName)))
-            {
-                connection.Open();
-                using (var command = new QC.SqlCommand())
-                {
-                    command.Connection = connection;
-                    command.CommandType = DT.CommandType.Text;
-                    command.CommandText = @"  
-select top 10 name from persons1 
-where name like @name
-";
-
-                    command.Parameters.AddWithValue("@name", Denormalize(term) + "%");
-
-                    QC.SqlDataReader reader = command.ExecuteReader();
-
-                    while (reader.Read())
-                    {
-                        autocompleteItems.Add(NormalizeString(reader.GetString(0)));
-                    }
-                }
-            }
-
-            return Json(autocompleteItems.ToArray(), JsonRequestBehavior.AllowGet);
-        }
+        #endregion
     }
 }
---
+++ ExploreWiki/Scripts/exploreWiki.js
@@ -0,0 +1,103 @@
+﻿$(document).ready(function () {
+    $.curCSS = function (element, attrib, val) {
+        $(element).css(attrib, val);
+    };
+
+    var canvas = document.getElementsByTagName('canvas')[0];
+    canvas.height = 800;
+    canvas.style.width = '800px';
+    canvas.style.height = '800px';
+
+    var container = document.getElementById('mynetwork');
+    
+    // TO DO populating DATA object: nodes/edges can (and should) be populated on CLIENT(not on Server side)
+    // Reason: better perfomance
+    // The BEST way: 
+    // API Controler for feching data which will return JsonResult. JsonResult, fatched with AJAX calls sould be then deserialized (PersonNames => nodes & PersonConnections => edges) and GRAPH should be generated on CLIENT side   
+    // The EASIER way: 
+    // Existing controller action can pass serialized objects (ViewBag.PersonsJson & ViewBag.PersonsConnectionsJson) to View which will be pased as HTML data-attributes to client side and GRAPH should be generated on CLIENT side  
+
+    //Example:
+    var personNamesJson = $("#container").attr("data-personNamesJson");
+    var personConnectionsJson = $("#container").attr("data-personsConnectionsJson");
+    var searchTermsViaAttribute =$("#container").attr("data-searchTerm");
+
+    // TO DO create an array with nodes from serialized JSONs on CLIENT side...
+    // var nodes = new vis.DataSet([]);
+    // create an array with edges
+    // var edges = new vis.DataSet([]);
+      
+
+    var data = {
+        nodes: nodes,
+        edges: edges
+    };
+
+    var options = {
+        // layout: {
+        //     hierarchical: {
+        //         sortMethod: "directed"
+        //     }
+        // },
+        physics: {
+            enabled: true,
+            "barnesHut": {
+                "gravitationalConstant": -10,
+                "centralGravity": 0.0,
+                "damping": 1,
+                "avoidOverlap": 1,
+                "springLength": 450
+                //,"nodeDistance": 400
+            },
+            "maxVelocity": 0.5,
+            "minVelocity": 0,
+            "timestep": 0.01,
+            repulsion: {
+                centralGravity: 0.0,
+                springLength: 500,
+                springConstant: 0.05,
+                nodeDistance: 500,
+                damping: 0.09
+            },
+            stabilization: {
+                enabled: true,
+                iterations: 100,
+                updateInterval: 0.01,
+                onlyDynamicEdges: false,
+                fit: true
+            },
+        },
+        layout: { improvedLayout: true }
+    };
+    var network = new vis.Network(container, data, options);
+
+
+    // TODO: wiki links point to local domain. This should be changed here.
+    // Either redirect to wiki or parhaps do search on explorewiki.
+    var url = "http://en.wikipedia.org/w/api.php?action=parse&format=json&page=" + searchTerm + "&redirects&prop=text&callback=?";
+    $.getJSON(url,
+        function(data) {
+            var wikiHTML = "";
+            if (data.error === undefined)
+                wikiHTML = data.parse.text["*"];
+            $wikiDOM = $("<document>" + wikiHTML + "</document>");
+            $("#wikiboxshow").html($wikiDOM.find('.infobox').html());
+        });
+
+    network.on("click",
+        function(params) {
+            var node = nodes.get(params.nodes[0]);
+            var searchTerm = node.label;
+            var url = "http://en.wikipedia.org/w/api.php?action=parse&format=json&page=" +
+                searchTerm +
+                "&redirects&prop=text&callback=?";
+            $.getJSON(url,
+                function(data) {
+                    var wikiHTML = "";
+                    if (data.error === undefined)
+                        wikiHTML = data.parse.text["*"];
+                    $wikiDOM = $("<document>" + wikiHTML + "</document>");
+                    $("#wikiboxshow").html($wikiDOM.find('.infobox').html());
+                });
+        });
+});
